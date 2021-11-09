989228345fb1b168c19fe39fb37c29c948a52e8e
WenConrad <conradwen@gmail.com>
2021-11-08 17:05:37 -0800

    use classes to create objects

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
-
-  commit() {
-    balance -= this.amount;
+  get balance() {
+  	let balance = 0;
+    for (let i of this.transactions) {
+    	balance += i.value;
+    }
+    return balance;
   }
-
+  addTransaction(transaction) {
+  	this.transactions.push(transaction);
+  }
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+  commit() {
+    if (!this.isAllowed()) {
+      console.log(`Insufficient funds for $${this.amount} ${this.constructor.name}. Remaining balance: $${this.account.balance}.`);
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    console.log(`$${this.amount} ${this.constructor.name}. Success! New balance: ${this.account.balance}`)
+    return true;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+    return (this.account.balance >= this.amount);
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    let approved = true;
+    return this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("C");
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const t1 = new Deposit(13, myAccount);
+const t2 = new Withdrawal(10, myAccount);
+const t3 = new Withdrawal(1, myAccount);
+const t4 = new Withdrawal(10, myAccount);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log(t1.commit(), myAccount.balance);
+console.log(t2.commit(), myAccount.balance);
+console.log(t3.commit(), myAccount.balance);
+console.log(t4.commit(), myAccount.balance);
